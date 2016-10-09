a62a2a2c05f4808c8cdcb29f3e939a97bc645a71
Valentin <valentin-bouquet@hotmail.fr>
2016-10-09 18:17:31 +0200

    Add tests for overloading method play to setup a game for 2players

--- tests/fr/p10/miage/rps/model/RockPaperScissorsTest.java
+++ tests/fr/p10/miage/rps/model/RockPaperScissorsTest.java
@@ -1,6 +1,9 @@
 package fr.p10.miage.rps.model;
 
 import org.testng.annotations.*;
+import org.testng.internal.junit.ArrayAsserts;
+
+import java.util.ArrayList;
 
 import static org.testng.Assert.*;
 
@@ -21,7 +24,124 @@ public class RockPaperScissorsTest {
         rps = null;
     }
 
+    @DataProvider(name="winData")
+    public Object[][] createWinData() {
+        return new Object [][] {
+                {RPSEnum.ROCK, RPSEnum.SCISSORS},
+                {RPSEnum.SCISSORS, RPSEnum.PAPER},
+                {RPSEnum.PAPER, RPSEnum.ROCK}
+        };
+    }
+
+    @Test(dataProvider = "winData")
+    void testWinPlayDataProvider(RPSEnum p1, RPSEnum p2) {
+        assertEquals(rps.play(p1, p2), Result.WIN);
+    }
+
+    @DataProvider(name="lostData")
+    public Object[][] createLostData() {
+        return new Object [][] {
+                {RPSEnum.SCISSORS, RPSEnum.ROCK},
+                {RPSEnum.PAPER, RPSEnum.SCISSORS},
+                {RPSEnum.ROCK, RPSEnum.PAPER}
+        };
+    }
+
+    @Test(dataProvider = "lostData")
+    void testLostDataProvider(RPSEnum p1, RPSEnum p2) {
+        assertEquals(rps.play(p1, p2), Result.LOST);
+    }
+
+    @DataProvider(name="tieData")
+    public Object[][] createTieData() {
+        return new Object [][] {
+                {RPSEnum.ROCK, RPSEnum.ROCK},
+                {RPSEnum.SCISSORS, RPSEnum.SCISSORS},
+                {RPSEnum.PAPER, RPSEnum.PAPER}
+        };
+    }
+
+    @Test(dataProvider = "tieData")
+    void testTieDataProvider(RPSEnum p1, RPSEnum p2) {
+        assertEquals(rps.play(p1, p2), Result.TIE);
+    }
+
+    @DataProvider(name="playerWin")
+    public Object[][] createWinGame() {
+        return new Object [][]{
+                {new Player("Jean", new ArrayList<RPSEnum>() {{
+                    add(RPSEnum.PAPER);
+                    add(RPSEnum.ROCK);
+                    add(RPSEnum.SCISSORS);
+                }}),
+                        new Player("Jean", new ArrayList<RPSEnum>() {{
+                            add(RPSEnum.PAPER);
+                            add(RPSEnum.SCISSORS);
+                            add(RPSEnum.PAPER);
+                        }})
+                }
+        };
+    }
+
+    @Test(dataProvider = "playerWin")
+    void testWinGame(Player p1, Player p2) throws Exception{
+        assertEquals(rps.play(p1, p2), Result.WIN);
+    }
+
+    @DataProvider(name="playerLost")
+    public Object[][] createLostGame() {
+        return new Object [][]{
+                {new Player("Jean", new ArrayList<RPSEnum>() {{
+                    add(RPSEnum.SCISSORS);
+                    add(RPSEnum.PAPER);
+                    add(RPSEnum.SCISSORS);
+                }}),
+                        new Player("Jean", new ArrayList<RPSEnum>() {{
+                            add(RPSEnum.ROCK);
+                            add(RPSEnum.PAPER);
+                            add(RPSEnum.ROCK);
+                        }})
+                }
+        };
+    }
+
+    @Test(dataProvider = "playerLost")
+    void testLostGame(Player p1, Player p2) throws Exception{
+        assertEquals(rps.play(p1, p2), Result.LOST);
+    }
+
+    @DataProvider(name="playerTie")
+    public Object[][] createTieGame() {
+        return new Object [][]{
+                {new Player("Jean", new ArrayList<RPSEnum>() {{
+                    add(RPSEnum.PAPER);
+                    add(RPSEnum.ROCK);
+                    add(RPSEnum.ROCK);
+                }}),
+                        new Player("Jean", new ArrayList<RPSEnum>() {{
+                            add(RPSEnum.SCISSORS);
+                            add(RPSEnum.ROCK);
+                            add(RPSEnum.SCISSORS);
+                        }})
+                }
+        };
+    }
+
+    @Test(dataProvider = "playerTie")
+    void testTieGame(Player p1, Player p2) throws Exception{
+        assertEquals(rps.play(p1, p2), Result.TIE);
+    }
     /*
+    void testLostPlay(Player p1, Player p2) throws Exception{
+        assertEquals(rps.play(p1, p2), Result.LOST);
+    }
+
+    void testTiePlay(Player p1, Player p2) throws Exception{
+        assertEquals(rps.play(p1, p2), Result.TIE);
+    }
+    */
+
+     /*
     @Parameters({"rock", "rock"})
     @Test
     public void testTIEPlay(String p1, String p2) throws Exception {
@@ -76,48 +196,6 @@ public class RockPaperScissorsTest {
         assertEquals(rps.play(RPSEnum.valueOf(p1), RPSEnum.valueOf(p2)), Result.LOST);
     }
     */
-
-    @DataProvider(name="winData")
-    public Object[][] createWinData() {
-        return new Object [][] {
-                {RPSEnum.ROCK, RPSEnum.SCISSORS},
-                {RPSEnum.SCISSORS, RPSEnum.PAPER},
-                {RPSEnum.PAPER, RPSEnum.ROCK}
-        };
-    }
-
-    @DataProvider(name="lostData")
-    public Object[][] createLostData() {
-        return new Object [][] {
-                {RPSEnum.SCISSORS, RPSEnum.ROCK},
-                {RPSEnum.PAPER, RPSEnum.SCISSORS},
-                {RPSEnum.ROCK, RPSEnum.PAPER}
-        };
-    }
-
-    @DataProvider(name="tieData")
-    public Object[][] createTieData() {
-        return new Object [][] {
-                {RPSEnum.ROCK, RPSEnum.ROCK},
-                {RPSEnum.SCISSORS, RPSEnum.SCISSORS},
-                {RPSEnum.PAPER, RPSEnum.PAPER}
-        };
-    }
-
-    @Test(dataProvider = "winData")
-    void testWinPlayDataProvider(RPSEnum p1, RPSEnum p2) {
-        assertEquals(rps.play(p1, p2), Result.WIN);
-    }
-
-    @Test(dataProvider = "lostData")
-    void testLostDataProvider(RPSEnum p1, RPSEnum p2) {
-        assertEquals(rps.play(p1, p2), Result.LOST);
-    }
-
-    @Test(dataProvider = "tieData")
-    void testTieDataProvider(RPSEnum p1, RPSEnum p2) {
-        assertEquals(rps.play(p1, p2), Result.TIE);
-    }
 
 }
 
